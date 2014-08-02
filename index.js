183f6db7abc6fcc87e7574de5da4714f64b52fd8
Nicholas Penree <nick@penree.com>
2014-08-02 12:36:05 -0400

    Switch to alfredo module

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var path = require('path');
-var alp = require('alp');
-var fuzzy = require('fuzzy-filter');
+var alfredo = require('alfredo');
 
-var PROJECTS_FILE = process.env.HOME + '/Library/Application Support/Chocolat/recent_projects.plist';
+var PROJECTS_FILE = path.join(
+  process.env.HOME,
+  'Library',
+  'Application Support',
+  'Chocolat',
+  'recent_projects.plist'
+);
 
-exports.getResults = function(arg) {
-  var projects = alp.readPlistSync(PROJECTS_FILE) || [];
+var getResults = exports.getResults = function(arg) {
+  var projects = alfredo.readPlistSync(PROJECTS_FILE) || [];
   var results = [];
   
   if (!arg) {
@@ -13,7 +18,7 @@ exports.getResults = function(arg) {
       return b.lastOpened - a.lastOpened;
     });
   } else {
-    return fuzzy(arg, projects.map(function(project) {
+    return alfredo.fuzzy(arg, projects.map(function(project) {
       return project.directoryPath;
     })).map(function(name) {
       return projects.filter(function(project) {
@@ -25,22 +30,18 @@ exports.getResults = function(arg) {
   return results || [];
 };
 
-exports.getItems = function(arg) {
-  var items = [];
-  
-  exports.getResults(arg).forEach(function(project) {
-    items.push(new alp.Item({
+var getItems = exports.getItems = function(arg) {
+  return getResults(arg).map(function(project) {
+    return new alfredo.Item({
       title: path.basename(project.directoryPath),
       subtitle: project.directoryPath,
       arg: project.directoryPath
-    }));
+    });
   });
-  
-  return items;
 };
 
 if (!require.parent) {
   var args = process.argv;
-  var items = exports.getItems(args.length >= 3 ? args[2] : undefined);
-  alp.feedback(items);
+  var items = getItems(args.length >= 3 ? args[2] : undefined);
+  alfredo.feedback(items);
 }
